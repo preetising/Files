0e681a990545394c8c97033fb3e8984e0904c916
preetising <84845068+preetising@users.noreply.github.com>
2021-07-11 20:51:12 +0530

    Add files via upload

---
+++ w3-5.py
@@ -0,0 +1,42 @@
+my_file=open("w3-5.txt","r")
+a=my_file.readlines()
+print(a)
+my_file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# char=input("enter the character.......")
+# my_file=open("w3-5.txt","r")
+# a=my_file.read()
+# i=0
+# while i<a:
+#     if i<char:
+#         print("largest")
+#     elif i>char:
+#         print("less words")
+#     else:
+#         print("notning")
+# my_file.close()
+
+
+
+# char=input("enter the character...")
+# a=input("enter the chacter...")
+# if a<char:
+#     print("lagtest word ")
+# elif a>char:
+#     print("less words")
+# else:
+#     pass
